47f84cbc263c00833a9a6d43dbeacc51517b4179
tu21897 <tunguyen2189721897@yahoo.com>
2022-04-19 13:48:05 -0700

    comments and bug fixes

--- data_clean.py
+++ data_clean.py
@@ -14,40 +14,85 @@ import numpy as np
 #   remove stray, obviously invalid data
 
 # The read csv file
-read = 'raw_hr_hr_sm.csv'
+read = 'raw_hr_hr.csv'
 # The write csv file
 write = 'cleaned_' + read
 # The csv file with as a data frame
-df = pd.read_csv(read)
+dataframe = pd.read_csv(read)
 # Suppress numpy float sci form
 np.set_printoptions(suppress=True)
 
 def main():
-    # resulting merged & cleaned data frame
+    # resulting cleaned data frame
     dest_map = {}
     
-    dest_map.update(conv_to_pacific(df, 'start', 'datetime'))
-    dest_map.update(conv_hr_to_int(df, 'value', 'heartrate'))
-    dest_map.update(conv_dur_to_int(df, 'duration', 'duration'))
+    # convert the datetime data to pacific time
+    dest_map.update(conv_to_pacific(dataframe, 'start', 'datetime'))
+    # convert the heart rate data to single integers
+    dest_map.update(conv_hr_to_int(dataframe, 'value', 'heartrate'))
+    # convert the duration data to integers
+    dest_map.update(conv_dur_to_int(dataframe, 'duration', 'duration'))
 
-    dest_df = pd.DataFrame.from_dict(dest_map)
+    # drop all rows with invalid data
+    dest_df = rows_drop_invalid(pd.DataFrame.from_dict(dest_map), [int, str])
+
+    # output results to write csv
     dest_df.to_csv(write, float_format='%.3f', index=False)
 
+# Drops all rows that do not have the specified types
+# returns the resulting data frame
+#
+# df - the data frame modified
+# types - the types to be checked in each row
+def rows_drop_invalid(df, types):
+    rows = df.to_numpy()
+    for i in range(len(rows)):
+        for item in rows[i]:
+            if (type(item) not in types):
+                df = df.drop(i)
+                break
+    return df
+
+# Converts duration data to integers, sums if there are
+# multiple integers, returns the resulting column
+#
+# df - the data frame being accessed
+# colname - the column name being accessed
+# newcolname - the returned column name
 def conv_dur_to_int(df, colname, newcolname):
     c = df[colname].to_numpy()
     col = [np.fromstring(c[i].replace('[', '').replace(']', '').replace(',', ' '), dtype=int, sep= ' ') for i in range(len(c))]
     return {newcolname: {i:np.sum(col[i], dtype=int) for i in range(len(col))}}
 
+# Converts heart rate data to integers, averages if there are
+# multiple integers, returns the resulting column
+#
+# df - the data frame being accessed
+# colname - the column name being accessed
+# newcolname - the returned column name
 def conv_hr_to_int(df, colname, newcolname):
     c = df[colname].to_numpy()
     col = [np.fromstring(c[i].replace('[', '').replace(']', '').replace(',', ' '), dtype=int, sep= ' ') for i in range(len(c))]
     return {newcolname: {i:np.mean(col[i], dtype=int) for i in range(len(col))}}
 
+# Converts timedate data to pacific time, trims 
+# timezone identifier, returns the resulting column
+#
+# df - the data frame being accessed
+# colname - the column name being accessed
+# newcolname - the returned column name
 def conv_to_pacific(df, colname, newcolname):
     col = df[colname].to_numpy()
     dati = pd.to_datetime(col, utc=True).astype('datetime64[ns, US/Pacific]').astype('str')
     return {newcolname: {i:dati[i][:len(dati[i])-6] for i in range(len(dati))}}
 
+# Splits a column on spliton and returns the resulting
+# columns with new column names
+#
+# df - the data frame being accessed
+# colname - the column name being accessed
+# newcolnames - the returned column names
+# spliton - the character to split on 
 def cols_split(df, colname, newcolnames, spliton):
     col = df[colname]
     cols = []
@@ -56,6 +101,11 @@ def cols_split(df, colname, newcolnames, spliton):
     cols = np.array(cols).transpose()
     return {newcolnames[i]: {j:cols[i][j] for j in range(len(cols[i]))} for i in range(len(newcolnames))}
 
+# Prints n rows of data in the format:
+# col1: data, col2: data, ...
+#
+# df - the data frame being accessed
+# n - the number of rows to print
 def print_n_rows_info(df, n):
     rows = df.to_numpy()
     cols = df.columns.to_numpy()
@@ -69,7 +119,10 @@ def print_n_rows_info(df, n):
         print(row[:len(row)-2])
         row = ""
 
-def print_cols(df):
+# Prints all the column names in the data frame
+#
+# df - the data frame being accessed
+def print_col_names(df):
     colnames = df.columns.to_numpy()
     for i in range(len(colnames)):
         print(colnames[i])
